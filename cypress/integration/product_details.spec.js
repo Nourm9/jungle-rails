3b83c7fba01b102effd7d0fd80dcab21bf2e938d
Nourm9 <nourmohamoud9@gmail.com>
2023-06-22 22:36:58 +0000

    added product cypress test

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,11 @@
+describe("product details page", () => {
+  it("There is a homepage", () => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+
+});
